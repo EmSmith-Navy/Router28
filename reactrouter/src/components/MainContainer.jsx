51d9063c1633c02a72e81699f0e6f39dacab3d47
EmSmith-Navy <keino2586@gmail.com>
2024-11-07 09:37:57 -0600

    Initial commit

---
+++ reactrouter/src/components/MainContainer.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Red from "./Red";
+import Blue from "./Blue";
+import Green from "./Green";
+
+export default function MainContainer() {
+  return (
+    <div id="main-section">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blue" element={<Blue />} />
+        <Route path="/red" element={<Red />} />
+        <Route path="/green" element={<Green />} />
+      </Routes>
+    </div>
+  );
+}
